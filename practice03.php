b0f482c3bfaa92da9ed38518f0f3fc8b663babee
sota <mtnsot919@icloud.com>
2023-12-29 11:15:54 +0000

    課題3

--- practice03.php
+++ practice03.php
@@ -8,3 +8,43 @@
 // $fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください
 
 // 【応用】下記のプログラムのバグを修正し、1から100までの間で5の倍数のみ表示するようにしてみてください
+
+$name = "SotaMatsuno";
+
+if ($name = "SotaMatsuno") {
+    echo "私は あなたの名前 です";
+} else {
+    echo "あなたの名前ではありません";
+}
+echo "\n";
+
+
+$total = 0;
+echo $total;
+echo "\n";
+
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+
+$fruits = array("orange","apple","grape","lemon","cherry");
+
+foreach($fruits as $fruits){
+    echo "要素は" . $fruits;
+    echo "\n";
+}
+
+//* for文の始めの値を定義する //
+$start = 0;
+
+for($i = 1; $i <= 100; $i++){
+    $start = $i;
+  // 5で割り切れたら{}内を実行する
+  if($start % 5 === 0){
+    echo $start;
+    echo "\n";
+  }
+}
